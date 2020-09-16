de3588439d97f6be6321a9ef2f6c03d8d94cd5a1
Artur Strug <artstrug@gmail.com>
2020-09-16 22:47:56 +0200

    Add basic login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,8 @@
-const { describe } = require("mocha");
+//const { describe } = require("mocha");
 
 describe('When Conduit page has been opened', function() {
     before(() => {
-        cy.visit('https://react-redux.realworld.io/#/')
+        cy.visit('https://react-redux.realworld.io/#/login')
     });
 
     it('then page title is correct', () => {
@@ -12,6 +12,12 @@ describe('When Conduit page has been opened', function() {
 
 describe('When valid credentials has been entered', function() {
     before(() => {
-        cy.get()
+        cy.get('input[type="email"]').type('as@ultramail.com')
+        cy.get('input[type="password"]').type('qwer123!')
+        cy.get('button[type="submit"]').click()
+    });
+
+    it('then correct user name should be displayed', () => {
+        cy.get('a[href="#@arthurs"]').should('be.visible')
     });
 })
