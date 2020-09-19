a3a45c5cf9f913af78cf5a0bf70251490d41a3e7
Artur Strug <artstrug@gmail.com>
2020-09-20 00:21:01 +0200

    Add sign up tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,17 +11,28 @@ Cypress.Commands.add('enterLoginData', (email, password) => {
     cy.get('input[type="password"]').type(password)
 })
 
-Cypress.Commands.add('submitLoginForm', () => {
-    cy.get('button[type="submit"]').click()
+Cypress.Commands.add('enterSignUpData', (username, email, password) => {
+    if (cy.get('h1').contains('Sign up').should('not.be.visible')) {
+        cy.get('.nav-link').contains('Sign up').click()
+    }
+    cy.get('input[placeholder="Username"]').type(username)
+    cy.get('input[type="email"]').type(email)
+    cy.get('input[type="password"]').type(password)
 })
 
-Cypress.Commands.add('clearLoginInputs', () => {
-    cy.get('input[type="email"]').clear()
-    cy.get('input[type="password"]').clear()
+Cypress.Commands.add('clearInputFields', () => {
+    cy.get('input').each(($input) => {
+        cy.wrap($input).clear()
+    })
 })
 
-Cypress.Commands.add('checkLogInErrorMsg', () => {
-    cy.get('.error-messages')
+Cypress.Commands.add('checkErrorMsg', (text, index = 0) => {
+    cy.get('.error-messages li')
+        .eq(index)
         .should('be.visible')
-        .should('have.text', 'email or password is invalid')
+        .should('have.text', text)
+})
+
+Cypress.Commands.add('submitForm', () => {
+    cy.get('form').submit()
 })
